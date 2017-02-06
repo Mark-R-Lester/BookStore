58c2b13cf8f7d066a543fae77886c19bbbec1ba3
Mark-R-Lester <mrlester@live.co.uk>
2017-02-06 16:18:00 +0000

    Mark3

--- src/store/Book.java
+++ src/store/Book.java
@@ -2,8 +2,32 @@ package store;
 
 public class Book {
 
-	String author = "";
-	String title = "";
-	int numberOfPages = 100;
+	private String author = "";
+	private String title = "";
+	private int numberOfPages = 100;
+	
+	
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+	public int getNumberOfPages() {
+		return numberOfPages;
+	}
+	public void setNumberOfPages(int numberOfPages) {
+		this.numberOfPages = numberOfPages;
+	}
 	
 }
--- src/store/Service.java
+++ src/store/Service.java
@@ -1,5 +1,37 @@
 package store;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+
 public class Service {
 
+	Map<String, Book> store = new HashMap<>();
+	
+	public void addBook(Book book){
+		store.put(book.getTitle(), book);
+	}
+	
+	public void deleteBook(String title){
+		store.remove(title);
+	}
+	
+	
+	public int quatityOfBooks(){
+		return store.size();
+	}
+	
+	public List<String> listBookNames(){
+		List<String> books= new ArrayList<>();
+		Iterator<Map.Entry<String, Book>> iter = store.entrySet().iterator();
+		while(iter.hasNext()){
+			books.add(iter.next().getKey());
+		}
+		return books;	
+	}
+	
+	
 }
